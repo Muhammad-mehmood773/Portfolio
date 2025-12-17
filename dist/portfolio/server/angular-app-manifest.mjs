b3a29f116a186aa733392526e9a08050f2337630
Muhammad-Mehmood773 <muhammadmehmood773@gmail.com>
2025-12-17 14:23:30 +0500

    new changesssss

--- dist/portfolio/server/angular-app-manifest.mjs
+++ dist/portfolio/server/angular-app-manifest.mjs
@@ -2,24 +2,24 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/Deploy-Portfolio/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/Deploy-Portfolio"
   },
   {
     "renderMode": 2,
-    "redirectTo": "/",
-    "route": "/**"
+    "redirectTo": "/Deploy-Portfolio",
+    "route": "/Deploy-Portfolio/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2194, hash: '1c2b8e68bc822f907ce21f95d0b35877c4bf5ed4fdd428bd8c6961954542cf2d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 969, hash: 'e60a0e2ea07eea1415c8bb602b209c38d2dbe2ed60cd5d93d29f361a31e79d7e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 15534, hash: 'dac013d8250011daca3b0c7503383245cd5d12a4ea8bde8a71995dedef27625b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2211, hash: '63ebdfd3ba1cc8052d98cf65fc03763772ea5801fb45b625f0bb64971ba96522', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 986, hash: 'ce9cd2a9a58dba48f497e0a5ec21d945c97b359247d1e53d37b45c74cd791087', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 15551, hash: 'c375d5113cdd1a237e3b214ad2dfe376646fd15dd2d0448318c5b86a9a56c150', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-JJFX674H.css': {size: 25540, hash: 'trdSKMnnmPg', text: () => import('./assets-chunks/styles-JJFX674H_css.mjs').then(m => m.default)}
   },
 };
