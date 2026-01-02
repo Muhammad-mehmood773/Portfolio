ab4991b728c96761a817a6ce0d88ec0d1e350069
Muhammad-Mehmood773 <muhammadmehmood773@gmail.com>
2026-01-02 17:13:57 +0500

    Deploy Angular build to GitHub Pages

--- dist/portfolio/server/angular-app-engine-manifest.mjs
+++ dist/portfolio/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/Deploy-Portfolio',
   supportedLocales: {
   "en-US": ""
 },
--- dist/portfolio/server/angular-app-manifest.mjs
+++ dist/portfolio/server/angular-app-manifest.mjs
@@ -2,24 +2,24 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/Deploy-Portfolio/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/Deploy-Portfolio"
   },
   {
     "renderMode": 2,
-    "redirectTo": "/",
-    "route": "/**"
+    "redirectTo": "/Deploy-Portfolio",
+    "route": "/Deploy-Portfolio/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2216, hash: 'bf3d41577c42068dc5ef7c965c5998cf6c62fff4ec3e8cfcb3831cfdfe0e9915', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 991, hash: '5bb420a2e5e3d02b00b0f9c7cedf028eb93b5c5507a25e87e5645f4f1e5c5b00', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 15556, hash: 'a46bfeced07b39ad6b0cf724b0fb495b5425e8c22d55e406e379f88cc34e891d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2233, hash: 'a06f9b6ad52ba19fddc26c3471f16fef04fd948821eda4d6d8f1618cdc426c67', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1008, hash: 'aa4a1d85a49565184ed1be6613e02a14104da381d2e16d1524a4f52076849e8b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 15573, hash: '0601f750747c7a744b474e2b2d408f82852f3ad1388045ea4b9bc3c22e75ea15', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-C7LIHTLL.css': {size: 27340, hash: 'S4radAMpvlw', text: () => import('./assets-chunks/styles-C7LIHTLL_css.mjs').then(m => m.default)}
   },
 };
